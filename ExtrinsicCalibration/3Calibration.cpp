dcb763fbf7e0ec8c819833a07f2721e01d744bc9
Yoni Smolin <Yoni.Smolin@gmail.com>
2015-12-26 09:12:30 +0200

    Extrinsic calibration of a pair of cameras (instead of 3 cameras as was before)

--- ExtrinsicCalibration/3Calibration.cpp
+++ ExtrinsicCalibration/3Calibration.cpp
@@ -1,5 +1,6 @@
 /*
 * 3calibration.cpp -- Calibrate 3 cameras in a horizontal line together.
+* This is a pretty crappy peace of code that I modified to support 2 cameras instead of 3.
 */
 
 #include "opencv2/calib3d/calib3d.hpp"
@@ -43,16 +44,10 @@ static void calcChessboardCorners(Size boardSize, float squareSize, vector<Point
 				float(i*squareSize), 0));
 }
 
-static bool run3Calibration(vector<vector<Point2f> > imagePoints1,
-	vector<vector<Point2f> > imagePoints2,
-	vector<vector<Point2f> > imagePoints3,
-	Size imageSize, Size boardSize,
-	float squareSize, float aspectRatio,
-	int flags,
-	Mat& cameraMatrix1, Mat& distCoeffs1,
-	Mat& cameraMatrix2, Mat& distCoeffs2,
-	Mat& cameraMatrix3, Mat& distCoeffs3,
-	Mat& R12, Mat& T12, Mat& R13, Mat& T13)
+static bool run3Calibration(vector<vector<Point2f> > imagePoints1,	vector<vector<Point2f> > imagePoints2,
+	Size imageSize, Size boardSize,	float squareSize, float aspectRatio,	int flags,
+	Mat& cameraMatrix1, Mat& distCoeffs1, Mat& cameraMatrix2, Mat& distCoeffs2,
+	Mat& R12, Mat& T12)
 {
 	int c, i;
 
@@ -62,9 +57,9 @@ static bool run3Calibration(vector<vector<Point2f> > imagePoints1,
 	calcChessboardCorners(boardSize, squareSize, objpt[0]);
 	vector<Mat> rvecs, tvecs;
 
-	for (c = 1; c <= 3; c++)
+	for (c = 1; c <= 2; c++)
 	{
-		const vector<vector<Point2f> >& imgpt0 = c == 1 ? imagePoints1 : c == 2 ? imagePoints2 : imagePoints3;
+		const vector<vector<Point2f> >& imgpt0 = c == 1 ? imagePoints1 : imagePoints2; 
 		imgpt.clear();
 		int N = 0;
 		for (i = 0; i < (int)imgpt0.size(); i++)
@@ -80,6 +75,8 @@ static bool run3Calibration(vector<vector<Point2f> > imagePoints1,
 			return false;
 		}
 
+		printf("Using a total of %zu images from camera %d for intrinsic calibration\n", imgpt.size(), c);
+
 		objpt.resize(imgpt.size(), objpt[0]);
 
 		Mat cameraMatrix = Mat::eye(3, 3, CV_64F);
@@ -90,69 +87,58 @@ static bool run3Calibration(vector<vector<Point2f> > imagePoints1,
 
 		double err = calibrateCamera(objpt, imgpt, imageSize, cameraMatrix,
 			distCoeffs, rvecs, tvecs,
-			flags | CALIB_FIX_K3/*|CALIB_FIX_K4|CALIB_FIX_K5|CALIB_FIX_K6*/);
+			flags | CALIB_FIX_K3/*|CALIB_FIX_K4|CALIB_FIX_K5|CALIB_FIX_K6*/); // err is RMS over all points - check out calib3d\src\calibration.cpp, line 1561
 		bool ok = checkRange(cameraMatrix) && checkRange(distCoeffs);
 		if (!ok)
 		{
 			printf("Error: camera %d was not calibrated\n", c);
 			return false;
 		}
-		printf("Camera %d calibration reprojection error = %g\n", c, sqrt(err / N));
+		printf("Camera %d calibration reprojection error = %g (intrinsic calibration)\n", c, err);
 
 		if (c == 1)
 			cameraMatrix1 = cameraMatrix, distCoeffs1 = distCoeffs;
-		else if (c == 2)
-			cameraMatrix2 = cameraMatrix, distCoeffs2 = distCoeffs;
 		else
-			cameraMatrix3 = cameraMatrix, distCoeffs3 = distCoeffs;
+			cameraMatrix2 = cameraMatrix, distCoeffs2 = distCoeffs;
 	}
 
 	vector<vector<Point2f> > imgpt_right;
 
-	// step 2: calibrate (1,2) and (3,2) pairs
-	for (c = 2; c <= 3; c++)
+	imgpt.clear();
+	imgpt_right.clear();
+	int N = 0;
+
+	for (i = 0; i < (int)std::min(imagePoints1.size(), imagePoints2.size()); i++)
+		if (!imagePoints1[i].empty() && !imagePoints2[i].empty())
+		{
+			if (imagePoints1[i].size() != imagePoints2[i].size()) throw std::runtime_error("Image point vectors must be of the same size");
+
+			imgpt.push_back(imagePoints1[i]);
+			imgpt_right.push_back(imagePoints2[i]);		
+			N += (int)imagePoints1[i].size();
+		}
+
+	if (imgpt.size() < 3)
 	{
-		const vector<vector<Point2f> >& imgpt0 = c == 2 ? imagePoints2 : imagePoints3;
-
-		imgpt.clear();
-		imgpt_right.clear();
-		int N = 0;
-
-		for (i = 0; i < (int)std::min(imagePoints1.size(), imgpt0.size()); i++)
-			if (!imagePoints1.empty() && !imgpt0[i].empty())
-			{
-				imgpt.push_back(imagePoints1[i]);
-				imgpt_right.push_back(imgpt0[i]);
-				N += (int)imgpt0[i].size();
-			}
-
-		if (imgpt.size() < 3)
-		{
-			printf("Error: not enough shared views for cameras 1 and %d\n", c);
-			return false;
-		}
-
-		objpt.resize(imgpt.size(), objpt[0]);
-		Mat cameraMatrix = c == 2 ? cameraMatrix2 : cameraMatrix3;
-		Mat distCoeffs = c == 2 ? distCoeffs2 : distCoeffs3;
-		Mat R, T, E, F;
-		double err = stereoCalibrate(objpt, imgpt, imgpt_right, cameraMatrix1, distCoeffs1,
-			cameraMatrix, distCoeffs,
-			imageSize, R, T, E, F,
-			CALIB_FIX_INTRINSIC,
-			TermCriteria(TermCriteria::COUNT, 30, 0));
-		printf("Pair (1,%d) calibration reprojection error = %g\n", c, sqrt(err / (N * 2)));
-		if (c == 2)
-		{
-			cameraMatrix2 = cameraMatrix;
-			distCoeffs2 = distCoeffs;
-			R12 = R; T12 = T;
-		}
-		else
-		{
-			R13 = R; T13 = T;
-		}
+		printf("Error: not enough shared views for cameras 1 and %d\n", c);
+		return false;
 	}
+
+	printf("Using a total of %zu images for extrinsic calibration\n", imgpt.size());
+
+	objpt.resize(imgpt.size(), objpt[0]);
+	Mat cameraMatrix = cameraMatrix2;
+	Mat distCoeffs = distCoeffs2;
+	Mat R, T, E, F;
+	double err = stereoCalibrate(objpt, imgpt, imgpt_right, cameraMatrix1, distCoeffs1,
+		cameraMatrix, distCoeffs,
+		imageSize, R, T, E, F,
+		CALIB_FIX_INTRINSIC,
+		TermCriteria(TermCriteria::COUNT, 30, 0)); // error is RMS over all points - check out calib3d\src\calibration.cpp, line 2092 
+	printf("Extrinsic calibration reprojection error = %g\n", err); 
+	cameraMatrix2 = cameraMatrix;
+	distCoeffs2 = distCoeffs;
+	R12 = R; T12 = T;
 
 	return true;
 }
@@ -182,7 +168,7 @@ int main(int argc, char** argv)
 	string outputFilename;
 	string inputFilename = "";
 
-	vector<vector<Point2f> > imgpt[3];
+	vector<vector<Point2f> > imgpt[2];
 	vector<string> imageList;
 
 	cv::CommandLineParser parser(argc, argv,
@@ -221,49 +207,50 @@ int main(int argc, char** argv)
 		return printf("Invalid aspect ratio\n"), -1;
 	if (inputFilename.empty() ||
 		!readStringList(inputFilename, imageList) ||
-		imageList.size() == 0 || imageList.size() % 3 != 0)
+		imageList.size() == 0 || imageList.size() % 2 != 0)
 	{
 		printf("Error: the input image list is not specified, or can not be read, or the number of files is not divisible by 3\n");
 		return -1;
 	}
 
 	Mat view, viewGray;
-	Mat cameraMatrix[3], distCoeffs[3], R[3], P[3], R12, T12;
-	for (k = 0; k < 3; k++)
+	Mat cameraMatrix[2], distCoeffs[2], R[2], P[2], R12, T12;
+	for (k = 0; k < 2; k++)
 	{
 		cameraMatrix[k] = Mat_<double>::eye(3, 3);
 		cameraMatrix[k].at<double>(0, 0) = aspectRatio;
 		cameraMatrix[k].at<double>(1, 1) = 1;
 		distCoeffs[k] = Mat_<double>::zeros(5, 1);
-	}
-	Mat R13 = Mat_<double>::eye(3, 3), T13 = Mat_<double>::zeros(3, 1);
+	}	
 
 	FileStorage fs;
-	namedWindow("Image View", 0);
+	//namedWindow("Image View", 0);
 
-	for (k = 0; k < 3; k++)
-		imgpt[k].resize(imageList.size() / 3);
+	for (k = 0; k < 2; k++)
+		imgpt[k].resize(imageList.size() / 2);
 
-	for (i = 0; i < (int)(imageList.size() / 3); i++)
+	for (i = 0; i < (int)(imageList.size() / 2); i++) // frame index
 	{
-		for (k = 0; k < 3; k++)
+		for (k = 0; k < 2; k++) // camera index
 		{
-			int k1 = k == 0 ? 2 : k == 1 ? 0 : 1;
-			printf("%s\n", imageList[i * 3 + k].c_str());
-			view = imread(imageList[i * 3 + k], 1);
+			auto imageIndex = i + k * imageList.size() / 2;
+			printf("%s\n", imageList[imageIndex].c_str());
+			view = imread(imageList[imageIndex], 1);
 
 			if (!view.empty())
 			{
-				vector<Point2f> ptvec;
+				vector<Point2f> ptvec(boardSize.height * boardSize.width); 
+				// the original code constructed an empty vector but this caused an assertion error at the end of the iteration.
+				// I don't have the time to debug this now, but there's probably some memory management issue that needs to be taken care of.
 				imageSize = view.size();
 				cvtColor(view, viewGray, COLOR_BGR2GRAY);
-				bool found = findChessboardCorners(view, boardSize, ptvec, CALIB_CB_ADAPTIVE_THRESH);
-
+				bool found = findChessboardCorners(view, boardSize, ptvec, CALIB_CB_ADAPTIVE_THRESH); // consider adding: CV_CALIB_CB_NORMALIZE_IMAGE
+				
 				drawChessboardCorners(view, boardSize, Mat(ptvec), found);
 				if (found)
 				{
-					imgpt[k1][i].resize(ptvec.size());
-					std::copy(ptvec.begin(), ptvec.end(), imgpt[k1][i].begin());
+					imgpt[k][i].resize(ptvec.size());
+					std::copy(ptvec.begin(), ptvec.end(), imgpt[k][i].begin()); // the discovered image plane points are written into the entry associated with the previous camera
 				}
 				//imshow("view", view);
 				//int c = waitKey(0) & 255;
@@ -275,86 +262,23 @@ int main(int argc, char** argv)
 
 	printf("Running calibration ...\n");
 
-	run3Calibration(imgpt[0], imgpt[1], imgpt[2], imageSize,
+	run3Calibration(imgpt[0], imgpt[1], imageSize,
 		boardSize, squareSize, aspectRatio, flags | CALIB_FIX_K4 | CALIB_FIX_K5,
 		cameraMatrix[0], distCoeffs[0],
 		cameraMatrix[1], distCoeffs[1],
-		cameraMatrix[2], distCoeffs[2],
-		R12, T12, R13, T13);
+		R12, T12);
 
 	fs.open(outputFilename, FileStorage::WRITE);
 
 	fs << "cameraMatrix1" << cameraMatrix[0];
 	fs << "cameraMatrix2" << cameraMatrix[1];
-	fs << "cameraMatrix3" << cameraMatrix[2];
 
 	fs << "distCoeffs1" << distCoeffs[0];
 	fs << "distCoeffs2" << distCoeffs[1];
-	fs << "distCoeffs3" << distCoeffs[2];
 
 	fs << "R12" << R12;
 	fs << "T12" << T12;
-	fs << "R13" << R13;
-	fs << "T13" << T13;
 
 	fs << "imageWidth" << imageSize.width;
 	fs << "imageHeight" << imageSize.height;
-
-	Mat Q;
-
-	// step 3: find rectification transforms
-	double ratio = rectify3Collinear(cameraMatrix[0], distCoeffs[0], cameraMatrix[1],
-		distCoeffs[1], cameraMatrix[2], distCoeffs[2],
-		imgpt[0], imgpt[2],
-		imageSize, R12, T12, R13, T13,
-		R[0], R[1], R[2], P[0], P[1], P[2], Q, -1.,
-		imageSize, 0, 0, CALIB_ZERO_DISPARITY);
-	Mat map1[3], map2[3];
-
-	fs << "R1" << R[0];
-	fs << "R2" << R[1];
-	fs << "R3" << R[2];
-
-	fs << "P1" << P[0];
-	fs << "P2" << P[1];
-	fs << "P3" << P[2];
-
-	fs << "disparityRatio" << ratio;
-	fs.release();
-
-	printf("Disparity ratio = %g\n", ratio);
-
-	for (k = 0; k < 3; k++)
-		initUndistortRectifyMap(cameraMatrix[k], distCoeffs[k], R[k], P[k], imageSize, CV_16SC2, map1[k], map2[k]);
-
-	Mat canvas(imageSize.height, imageSize.width * 3, CV_8UC3), small_canvas;
-	destroyWindow("view");
-	canvas = Scalar::all(0);
-
-	for (i = 0; i < (int)(imageList.size() / 3); i++)
-	{
-		canvas = Scalar::all(0);
-		for (k = 0; k < 3; k++)
-		{
-			int k1 = k == 0 ? 2 : k == 1 ? 0 : 1;
-			int k2 = k == 0 ? 1 : k == 1 ? 0 : 2;
-			view = imread(imageList[i * 3 + k], 1);
-
-			if (view.empty())
-				continue;
-
-			Mat rview = canvas.colRange(k2*imageSize.width, (k2 + 1)*imageSize.width);
-			remap(view, rview, map1[k1], map2[k1], INTER_LINEAR);
-		}
-		printf("%s %s %s\n", imageList[i * 3].c_str(), imageList[i * 3 + 1].c_str(), imageList[i * 3 + 2].c_str());
-		resize(canvas, small_canvas, Size(1500, 1500 / 3));
-		for (k = 0; k < small_canvas.rows; k += 16)
-			line(small_canvas, Point(0, k), Point(small_canvas.cols, k), Scalar(0, 255, 0), 1);
-		imshow("rectified", small_canvas);
-		int c = waitKey(0);
-		if (c == 27 || c == 'q' || c == 'Q')
-			break;
-	}
-
-	return 0;
 }
